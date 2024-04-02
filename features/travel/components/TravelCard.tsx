8417d70bfc02f12931244dd256e5b2c4341e794d
Hernan dP <hernandp@nimble.la>
2024-04-01 23:57:44 -0300

    User data

--- features/travel/components/TravelCard.tsx
+++ features/travel/components/TravelCard.tsx
@@ -6,6 +6,7 @@ import {
   Divider,
   HStack,
   IconButton,
+  Skeleton,
   Text,
   VStack,
   useTheme,
@@ -17,6 +18,8 @@ import { MaterialIcons } from "@expo/vector-icons";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { color } from "native-base/lib/typescript/theme/styled-system";
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
+import { DocumentReference } from "firebase/firestore";
 
 interface TravelCardProps {
   from?: string;
@@ -30,6 +33,10 @@ interface TravelCardProps {
     from: string;
     to: string;
   };
+  usercol?: DocumentReference<{
+    name: string;
+    carModel: string;
+  }>;
 }
 
 const TravelCard = ({
@@ -37,119 +44,127 @@ const TravelCard = ({
   to = "Cordoba",
   isSharingCost = false,
   isPetFriendly = false,
-  name = "Hernan de Prada",
-  carModel = "Onix",
+  usercol,
   date = "30 de Marzo",
   hourRange = { from: "10:00", to: "12:00" },
 }: TravelCardProps) => {
+  const [userData, loading] = useDocumentDataOnce(usercol);
+
   const { colors } = useTheme();
+
   return (
-    <VStack bg="white" py="5" px="5" rounded="2xl" space={3}>
-      <HStack alignItems="center">
-        <Avatar
-          bg="cyan.500"
-          source={{
-            uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-          }}
-        >
-          TE
-        </Avatar>
-        <VStack ml="3">
-          <Text fontSize="md" fontWeight="semibold">
-            {name}
-          </Text>
-          <Text fontSize="xs" color="gray.400" fontWeight="semibold">
-            {carModel}
-          </Text>
-        </VStack>
+    <Skeleton isLoaded={!loading && !!userData} h="48" rounded="2xl">
+      <VStack bg="white" py="5" px="5" rounded="2xl" space={3}>
+        <HStack alignItems="center">
+          <Avatar
+            bg="cyan.500"
+            source={{
+              uri: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+            }}
+          >
+            TE
+          </Avatar>
+          <VStack ml="3">
+            {userData && (
+              <>
+                <Text fontSize="md" fontWeight="semibold">
+                  {userData?.name}
+                </Text>
+                <Text fontSize="xs" color="gray.400" fontWeight="semibold">
+                  {userData?.carModel}
+                </Text>
+              </>
+            )}
+          </VStack>
 
-        <Button rounded="full" py="1" bg="primary.500" ml="auto">
-          Solicitar
-        </Button>
-      </HStack>
+          <Button rounded="full" py="1" bg="primary.500" ml="auto">
+            Solicitar
+          </Button>
+        </HStack>
 
-      <HStack alignItems="center">
-        <VStack>
+        <HStack alignItems="center">
+          <VStack>
+            <HStack alignItems="center" space={2}>
+              <FontAwesome name="genderless" size={24} color="lightblue" />
+              <Text fontSize="md" fontWeight="semibold">
+                {from}
+              </Text>
+            </HStack>
+
+            <HStack alignItems="center" space={2}>
+              <Feather name="map-pin" size={14} color="green" />
+              <Text fontSize="md" fontWeight="semibold">
+                {to}
+              </Text>
+            </HStack>
+          </VStack>
+
+          <HStack
+            alignSelf="flex-start"
+            bg="primary.500"
+            ml="auto"
+            px="2"
+            py="2"
+            rounded="sm"
+          >
+            <MaterialCommunityIcons name="car-seat" size={24} color="white" />
+            <Text color="white">3</Text>
+          </HStack>
+        </HStack>
+
+        <HStack justifyContent="space-between">
           <HStack alignItems="center" space={2}>
-            <FontAwesome name="genderless" size={24} color="lightblue" />
-            <Text fontSize="md" fontWeight="semibold">
-              {from}
+            <FontAwesome5 name="calendar" size={14} color="black" />
+            <Text fontSize="sm" fontWeight="semibold">
+              {date}
             </Text>
           </HStack>
 
           <HStack alignItems="center" space={2}>
-            <Feather name="map-pin" size={14} color="green" />
-            <Text fontSize="md" fontWeight="semibold">
-              {to}
+            <MaterialIcons name="access-time" size={14} color="black" />
+            <Text fontSize="sm" fontWeight="semibold">
+              {hourRange.from} - {hourRange.to}
             </Text>
           </HStack>
-        </VStack>
-
-        <HStack
-          alignSelf="flex-start"
-          bg="primary.500"
-          ml="auto"
-          px="2"
-          py="2"
-          rounded="sm"
-        >
-          <MaterialCommunityIcons name="car-seat" size={24} color="white" />
-          <Text color="white">3</Text>
-        </HStack>
-      </HStack>
-
-      <HStack justifyContent="space-between">
-        <HStack alignItems="center" space={2}>
-          <FontAwesome5 name="calendar" size={14} color="black" />
-          <Text fontSize="sm" fontWeight="semibold">
-            {date}
-          </Text>
         </HStack>
 
-        <HStack alignItems="center" space={2}>
-          <MaterialIcons name="access-time" size={14} color="black" />
-          <Text fontSize="sm" fontWeight="semibold">
-            {hourRange.from} - {hourRange.to}
-          </Text>
+        <Divider />
+
+        <HStack justifyContent="space-evenly" alignItems="center">
+          <VStack alignItems="center" flex="1">
+            <MaterialIcons
+              name="attach-money"
+              size={24}
+              color={isSharingCost ? colors.primary[500] : colors.gray[300]}
+            />
+          </VStack>
+
+          <Divider orientation="vertical" />
+
+          <VStack alignItems="center" flex="1">
+            <MaterialIcons
+              name="pets"
+              size={24}
+              color={isPetFriendly ? colors.primary[500] : colors.gray[300]}
+            />
+          </VStack>
+
+          <Divider orientation="vertical" />
+
+          <VStack alignItems="center" flex="1">
+            <IconButton
+              icon={
+                <Ionicons
+                  name="chatbox-ellipses-outline"
+                  size={24}
+                  color="black"
+                />
+              }
+            />
+          </VStack>
         </HStack>
-      </HStack>
-
-      <Divider />
-
-      <HStack justifyContent="space-evenly" alignItems="center">
-        <VStack alignItems="center" flex="1">
-          <MaterialIcons
-            name="attach-money"
-            size={24}
-            color={isSharingCost ? colors.primary[500] : colors.gray[300]}
-          />
-        </VStack>
-
-        <Divider orientation="vertical" />
-
-        <VStack alignItems="center" flex="1">
-          <MaterialIcons
-            name="pets"
-            size={24}
-            color={isPetFriendly ? colors.primary[500] : colors.gray[300]}
-          />
-        </VStack>
-
-        <Divider orientation="vertical" />
-
-        <VStack alignItems="center" flex="1">
-          <IconButton
-            icon={
-              <Ionicons
-                name="chatbox-ellipses-outline"
-                size={24}
-                color="black"
-              />
-            }
-          />
-        </VStack>
-      </HStack>
-    </VStack>
+      </VStack>
+    </Skeleton>
   );
 };
 
--- tabs/HomeTab.tsx
+++ tabs/HomeTab.tsx
@@ -1,35 +1,90 @@
-import { Flex, ScrollView, Text, VStack } from "native-base";
-import React, { useEffect, useState } from "react";
+import { ScrollView, VStack } from "native-base";
+import React, { useMemo } from "react";
 import { Fab } from "native-base";
 import Icon from "react-native-vector-icons/AntDesign";
 import TravelCard from "features/travel/components/TravelCard";
 import {
-  useCollection,
   useCollectionData,
-  useDocumentDataOnce,
-  useDocumentOnce,
+  useCollectionDataOnce,
+  useDocumentData,
 } from "react-firebase-hooks/firestore";
 import CalendarRow from "@components/CalendarRow";
-import { doc } from "firebase/firestore";
+import {
+  CollectionReference,
+  DocumentReference,
+  Timestamp,
+  collection,
+} from "firebase/firestore";
 import { db } from "firebase";
 import Loading from "@components/Loading";
+import dayjs from "dayjs";
 
-const docRef = doc(db, "travels", "nqJwIN92wfjE8MpuIk47");
+type Travel = {
+  creator: DocumentReference<{
+    name: string;
+    carModel: string;
+  }>;
+  departure: {
+    end: Timestamp;
+    start: Timestamp;
+  };
+  travel: {
+    from: string;
+    to: string;
+  };
+  isPetFriendly: boolean;
+  isSharingCost: boolean;
+};
+
+const transformDate = ({
+  end,
+  start,
+}: {
+  end: Timestamp;
+  start: Timestamp;
+}) => {
+  const startDate = dayjs(start.toDate()).locale("es").format("HH:mm");
+  const endDate = dayjs(end.toDate()).locale("es").format("HH:mm");
+
+  return {
+    start: startDate,
+    end: endDate,
+  };
+};
+
+const query = collection(db, "travels") as CollectionReference<Travel>;
 const HomeTab = () => {
-  const [value, loading, error] = useDocumentDataOnce(docRef);
+  const [travels, loading, error] = useCollectionDataOnce(query);
 
-  console.log({ loading });
-
-  if (loading) return <Loading />;
+  if (loading || !travels) return <Loading />;
 
   return (
     <ScrollView h="full" w="full" bg="#F0F4F7">
       <VStack py="5" px="5">
         <CalendarRow />
         <VStack space={5} mt="5">
-          <TravelCard isPetFriendly />
-          <TravelCard />
-          <TravelCard isSharingCost />
+          {travels.map((travel, index) => {
+            const departureDate = transformDate(travel.departure);
+            const departureDay = dayjs(travel.departure.start.toDate()).locale('es').format(
+              "DD [de] MMMM"
+            );
+
+            return (
+              <TravelCard
+                key={index}
+                isSharingCost={travel.isSharingCost}
+                isPetFriendly={travel.isPetFriendly}
+                from={travel.travel.from}
+                to={travel.travel.to}
+                hourRange={{
+                  from: departureDate.start,
+                  to: departureDate.end,
+                }}
+                date={departureDay}
+                usercol={travel.creator}
+              />
+            );
+          })}
         </VStack>
       </VStack>
       <Fab
