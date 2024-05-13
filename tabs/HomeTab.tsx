aec1dc3b748fa06a5deb3b2308a83ceed7e09240
Hernan dp <h.de.prada1@gmail.com>
2024-05-12 22:46:08 -0300

    Counter

--- tabs/HomeTab.tsx
+++ tabs/HomeTab.tsx
@@ -1,99 +1,10 @@
 import { Box, ScrollView, VStack } from "native-base";
 import React from "react";
-import { Fab } from "native-base";
-import Icon from "react-native-vector-icons/AntDesign";
-import TravelCard from "features/travel/components/TravelCard";
-import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
-import CalendarRow from "@components/CalendarRow";
-import {
-  CollectionReference,
-  DocumentReference,
-  Timestamp,
-  collection,
-} from "firebase/firestore";
-import { db } from "firebase";
-import Loading from "@components/Loading";
-import dayjs from "dayjs";
-import { useNavigation, useRoute } from "@react-navigation/native";
 
-type Travel = {
-  creator: DocumentReference<{
-    name: string;
-    carModel: string;
-  }>;
-  departure: {
-    end: Timestamp;
-    start: Timestamp;
-  };
-  travel: {
-    from: string;
-    to: string;
-  };
-  isPetFriendly: boolean;
-  isSharingCost: boolean;
-};
-
-const transformDate = ({
-  end,
-  start,
-}: {
-  end: Timestamp;
-  start: Timestamp;
-}) => {
-  const startDate = dayjs(start.toDate()).locale("es").format("HH:mm");
-  const endDate = dayjs(end.toDate()).locale("es").format("HH:mm");
-
-  return {
-    start: startDate,
-    end: endDate,
-  };
-};
-
-const query = collection(db, "travels") as CollectionReference<Travel>;
 const HomeTab = () => {
-  const [travels, loading] = useCollectionDataOnce(query);
-
-  if (loading || !travels) return <Loading />;
-
   return (
     <ScrollView h="full" w="full" bg="#F0F4F7">
-      <VStack py="5">
-        <Box pl="5">
-          <CalendarRow />
-        </Box>
-        <VStack space={5} mt="5" px="5">
-          {travels.map((travel, index) => {
-            const departureDate = transformDate(travel.departure);
-            const departureDay = dayjs(travel.departure.start.toDate())
-              .locale("es")
-              .format("DD [de] MMMM");
-
-            return (
-              <TravelCard
-                key={index}
-                isSharingCost={travel.isSharingCost}
-                isPetFriendly={travel.isPetFriendly}
-                from={travel.travel.from}
-                to={travel.travel.to}
-                hourRange={{
-                  from: departureDate.start,
-                  to: departureDate.end,
-                }}
-                date={departureDay}
-                usercol={travel.creator}
-              />
-            );
-          })}
-        </VStack>
-      </VStack>
-      <Fab
-        renderInPortal={true}
-        shadow={2}
-        bottom={20}
-        bg="primary.500"
-        size="sm"
-        icon={<Icon color="white" name="plus" size={25} />}
-      />
+      <VStack py="5"></VStack>
     </ScrollView>
   );
 };
